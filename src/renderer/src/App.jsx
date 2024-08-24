4f2478e169fff29d1f535d87cde1a1a1f0dddbc9
Rafael Guzman <rega1237@gmail.com>
2024-08-24 11:33:31 -0400

    Add ModalAccount component and handleToggleModal function

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,14 +1,22 @@
 import Header from './components/Header/Header'
 
 import patientPlaceholder from './assets/images/placeholder-user.webp'
+import ModalAccount from './components/Modal/ModalContainer'
+import { useState } from 'react'
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+
+  const handleToggleModal = () => {
+    setIsModalOpen(!isModalOpen)
+  }
 
   return (
     <>
       <div className="mx-auto w-full max-w-6xl p-4 sm:p-6 md:p-8">
-        <Header />
+        <Header handleToggleModal={handleToggleModal} />
 
         <div className="overflow-hidden rounded-lg border">
           <div className="relative w-full overflow-auto">
@@ -30,39 +38,33 @@ function App() {
                 <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
                   <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
                     <div className="flex items-center gap-3">
-                      <span class="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full">
+                      <span className="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full">
                         <img
-                          class="aspect-square h-full w-full"
+                          className="aspect-square h-full w-full"
                           alt="Patient Avatar"
                           src={patientPlaceholder}
                         />
                       </span>
                       <div>
-                        <p class="font-medium">Rafael Guzman</p>
-                        <p class="text-sm text-muted-foreground">19449831</p>
+                        <p className="font-medium">Rafael Guzman</p>
+                        <p className="text-sm text-muted-foreground">19449831</p>
                       </div>
                     </div>
                   </td>
-                  <td class="[&amp;:has([role=checkbox])]:pr-0 p-4 align-middle">
-                    <div class="flex flex-col gap-1">
+                  <td className="[&amp;:has([role=checkbox])]:pr-0 p-4 align-middle">
+                    <div className="flex flex-col gap-1">
                       <p data-id="28">0414-967-5141</p>
                     </div>
                   </td>
-                  <td class="[&amp;:has([role=checkbox])]:pr-0 p-4 align-middle" data-id="30">
-                    <div class="flex items-center gap-2" data-id="31">
-                      <button
-                        class="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-                      >
+                  <td className="[&amp;:has([role=checkbox])]:pr-0 p-4 align-middle" data-id="30">
+                    <div className="flex items-center gap-2" data-id="31">
+                      <button className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
                         Agendar Cita
                       </button>
-                      <button
-                        class="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-                      >
+                      <button className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
                         Crear Historia
                       </button>
-                      <button
-                        class="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
-                      >
+                      <button className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
                         Crear Sesión
                       </button>
                     </div>
@@ -73,6 +75,8 @@ function App() {
           </div>
         </div>
       </div>
+
+      {isModalOpen && <ModalAccount formType="newClient" handleToggleModal={handleToggleModal} />}
     </>
   )
 }
