af37c3ddb9f25b47f21c2c7ea676c5be6ce2e0d6
Rafael Guzman <rega1237@gmail.com>
2024-08-24 14:36:54 -0400

    Refactor App component to fetch and display pacientes data

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,13 +1,42 @@
-import Header from './components/Header/Header'
+import { useState, useEffect } from 'react'
+import { createClient } from '@libsql/client'
 
-import patientPlaceholder from './assets/images/placeholder-user.webp'
+import Header from './components/Header/Header'
+import TableBody from './components/Pacientes/TableBody'
 import ModalAccount from './components/Modal/ModalContainer'
-import { useState } from 'react'
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [pacientes, setPacientes] = useState([])
 
   const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+
+  useEffect(() => {
+    const storedPacientes = localStorage.getItem('pacientes')
+
+    if (storedPacientes) {
+      setPacientes(JSON.parse(storedPacientes))
+      return
+    }
+
+    const getPacientes = async () => {
+      try {
+        const pacientes = await turso.execute('SELECT * FROM pacientes')
+        setPacientes(pacientes.rows)
+        localStorage.setItem('pacientes', JSON.stringify(pacientes.rows))
+      } catch (error) {
+        alert('Error al obtener los pacientes')
+        console.error(error)
+      }
+    }
+
+    getPacientes()
+  }, [])
+
+  const turso = createClient({
+    url: import.meta.env.VITE_URL,
+    authToken: import.meta.env.VITE_API_KEY
+  })
 
   const handleToggleModal = () => {
     setIsModalOpen(!isModalOpen)
@@ -17,66 +46,17 @@ function App() {
     <>
       <div className="mx-auto w-full max-w-6xl p-4 sm:p-6 md:p-8">
         <Header handleToggleModal={handleToggleModal} />
-
-        <div className="overflow-hidden rounded-lg border">
-          <div className="relative w-full overflow-auto">
-            <table className="w-full caption-bottom text-sm">
-              <thead className="[&_tr]:border-b">
-                <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                  <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0">
-                    Nombre
-                  </th>
-                  <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground">
-                    Telefono
-                  </th>
-                  <th className="h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0">
-                    Acciones
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="[&_tr:last-child]:border-0">
-                <tr className="border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted">
-                  <td className="p-4 align-middle [&:has([role=checkbox])]:pr-0">
-                    <div className="flex items-center gap-3">
-                      <span className="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full">
-                        <img
-                          className="aspect-square h-full w-full"
-                          alt="Patient Avatar"
-                          src={patientPlaceholder}
-                        />
-                      </span>
-                      <div>
-                        <p className="font-medium">Rafael Guzman</p>
-                        <p className="text-sm text-muted-foreground">19449831</p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="[&amp;:has([role=checkbox])]:pr-0 p-4 align-middle">
-                    <div className="flex flex-col gap-1">
-                      <p data-id="28">0414-967-5141</p>
-                    </div>
-                  </td>
-                  <td className="[&amp;:has([role=checkbox])]:pr-0 p-4 align-middle" data-id="30">
-                    <div className="flex items-center gap-2" data-id="31">
-                      <button className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
-                        Agendar Cita
-                      </button>
-                      <button className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
-                        Crear Historia
-                      </button>
-                      <button className="inline-flex h-9 items-center justify-center whitespace-nowrap rounded-md border border-input bg-background px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
-                        Crear Sesión
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+        <TableBody pacientes={pacientes} />
       </div>
 
-      {isModalOpen && <ModalAccount formType="newClient" handleToggleModal={handleToggleModal} />}
+      {isModalOpen && (
+        <ModalAccount
+          formType="newClient"
+          handleToggleModal={handleToggleModal}
+          turso={turso}
+          setInfo={setPacientes}
+        />
+      )}
     </>
   )
 }
