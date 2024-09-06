4a985f9010a252d687ece20dd8d09f3f5f55258d
Rafael Guzman <rega1237@gmail.com>
2024-09-06 17:44:22 -0400

    Add new constants for body and facial forms

---
+++ src/renderer/src/Constants/bodyFormConstants.js
@@ -0,0 +1,31 @@
+export const alergias = {
+  Medicamentos: '',
+  Cosmeticos: '',
+  Perfumes: '',
+  Otros: ''
+}
+
+export const bodyClinicBackground = {
+  Diabetes: false,
+  Respiratorios: false,
+  Cardiacos: false,
+  Digestivos: false,
+  Estreñimiento: false,
+  Marcapasos: false,
+  Protesis: false,
+  'Ant Oncologicos': false,
+  Tiroides: false
+}
+
+export const motivoConsulta = {
+  'Flacidez Estrias o Celulitis': false,
+  'Cuidado de la piel': false,
+  'Tratamiento Fisioterapeutico': false,
+  'Estres/Ansiedad/Insomnio': false,
+  Otros: ''
+}
+
+export const antecedentesQuirurgicos = {
+  Implantes: false,
+  Cirugia: false
+}
---
+++ src/renderer/src/Constants/facialFormConstants.js
@@ -0,0 +1,62 @@
+export const facialHistoryClinicBackground = {
+  Diabetes: false,
+  Cancer: false,
+  Alergias: false,
+  Anemia: false,
+  Hipertensión: false,
+  'Enfermedades Renales': false,
+  'Medicamentos Actuales': ''
+}
+
+export const facialHistoryAestheticBackground = {
+  'Implante o injertos': false,
+  'Cirugias estéticas': false,
+  'Tratamientos estéticos no invasivos': false,
+  Botox: false,
+  'Acido hialuronico': false,
+  'Mesoterapia facial': false,
+  Dermapen: false,
+  'Hilos tensores': false,
+  Otros: ''
+}
+
+export const tipologiaCutanea = {
+  Seca: false,
+  Normal: false,
+  Grasa: false,
+  Mixta: false,
+  Sensible: false,
+  Asfixiada: false,
+  Deshidratada: false,
+  Desvitalizada: false,
+  Otro: ''
+}
+
+export const cuidadoPiel = {
+  Limpieza: '',
+  Hidratacion: '',
+  'Proteccion solar': '',
+  Exfoliacion: '',
+  'Limpieza profunda': '',
+  Mascarillas: '',
+  Otro: ''
+}
+
+export const patologiasCutaneas = {
+  Eritema: false,
+  Comedones: false,
+  Pustulas: false,
+  Cicatrices: false,
+  Papulas: false,
+  Millium: false,
+  Vitiligo: false,
+  Melasma: false,
+  Arrugas: false,
+  Efelides: false,
+  Hiperpigmentaciones: false,
+  Rosacea: false,
+  'Piel con tendencia acneica': false,
+  'Tipo de acne': '',
+  'Grado de acne': '',
+  'Causas del acne': ''
+}
