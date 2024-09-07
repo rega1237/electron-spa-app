92f21ab6e312dead654bca98915082e6da9ae5cd
Rafael Guzman <rega1237@gmail.com>
2024-09-07 08:28:35 -0400

    Add handleDate function to get current date

--- src/renderer/src/components/Forms/HistoryForm/handleFunctions.js
+++ src/renderer/src/components/Forms/HistoryForm/handleFunctions.js
@@ -13,3 +13,11 @@ export const handleSelect = (e, state, setState) => {
 export const handleTextArea = (e, setState) => {
   setState(e.target.value)
 }
+
+export const handleDate = () => {
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`
+}
