42b531efb5d4351afc90abf95834b8451bcd6e0d
Rafael Guzman <rega1237@gmail.com>
2024-09-10 18:44:21 -0400

    Refactor constant variable names in facialFormConstants.js

--- src/renderer/src/Constants/facialFormConstants.js
+++ src/renderer/src/Constants/facialFormConstants.js
@@ -63,33 +63,33 @@ export const patologiasCutaneas = {
 
 // const para las tables
 export const alergias_table = {
-  "alergias_medicamentos": "",
-  "alergias_cosmeticos": "",
-  "alergias_perfumes": "",
-  "alergias_otros": "",
+  alergias_medicamentos: '',
+  alergias_cosmeticos: '',
+  alergias_perfumes: '',
+  alergias_otros: ''
 }
 
 export const clinic_table = {
-  "clinic_diabetes": 0,
-  "clinic_respiratorios": 0,
-  "clinic_cardiacos": 0,
-  "clinic_digestivos": 0,
-  "clinic_estrenimiento": 0,
-  "clinic_marcapasos": 0,
-  "clinic_protesis": 0,
-  "clinic_ant_oncologicos": 0,
-  "clinic_tiroides": 0,
+  clinic_diabetes: 0,
+  clinic_respiratorios: 0,
+  clinic_cardiacos: 0,
+  clinic_digestivos: 0,
+  clinic_estrenimiento: 0,
+  clinic_marcapasos: 0,
+  clinic_protesis: 0,
+  clinic_ant_oncologicos: 0,
+  clinic_tiroides: 0
 }
-  
+
 export const motivo_table = {
-  "motivo_flacidez": 0,
-  "motivo_cuidado_piel": 0,
-  "motivo_tratamiento_fisioterapeutico": 0,
-  "motivo_estres": 0,
-  "motivo_otros": "",
+  motivo_flacidez: 0,
+  motivo_cuidado_piel: 0,
+  motivo_tratamiento_fisioterapeutico: 0,
+  motivo_estres: 0,
+  motivo_otros: ''
 }
 
 export const antecedentes_table = {
-  "ant_quirur_implantes": 0,
-  "ant_quirur_cirugia": 0,
+  ant_quirur_implantes: 0,
+  ant_quirur_cirugia: 0
 }
