8c433214f69b29d1d6772cc2a7cac6b20ca1bfaf
Rafael Guzman <rega1237@gmail.com>
2024-09-10 18:16:02 -0400

    Add edit and delete functionality to facialHistoryStore.js

--- src/renderer/src/store/facialHistoryStore.js
+++ src/renderer/src/store/facialHistoryStore.js
@@ -62,6 +62,45 @@ const useHistoriaFacial = create((set, get) => ({
       console.error(error.message)
       alert('Error al guardar la historia facial')
     }
+  },
+  editHistoriaFacial: async (data) => {
+    try {
+      const keys = Object.keys(data)
+        .filter((key) => key !== 'id' || key !== 'paciente_id' || key !== 'fecha_historia')
+        .map((key) => `${key} = :${key}`)
+        .join(', ')
+
+      const id = data.id
+   
+      const sqlAction = await turso.execute({
+        sql: `UPDATE historia_facial SET ${keys} WHERE id = ${id}`,
+        args: data
+      })
+
+      set((state) => ({ historiaFacial: data }))
+
+      const getHistoriaFacial = get().historiaFacial
+
+      localStorage.setItem('historiaFacial', JSON.stringify(getHistoriaFacial))
+    } catch (error) {
+      console.error(error.message)
+      alert('Error al editar la historia facial')
+    }
+  },
+  deleteHistoriaFacial: async (id) => {
+    try {
+      await turso.execute({
+        sql: 'DELETE FROM historia_facial WHERE id = :id',
+        args: { id }
+      })
+
+      set((state) => ({ historiaFacial: {} }))
+
+      localStorage.setItem('historiaFacial', JSON.stringify({}))
+    } catch (error) {
+      console.error(error.message)
+      alert('Error al eliminar la historia facial')
+    }
   }
 }))
 
