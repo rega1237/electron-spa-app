9be57c3676ff753623595e45b8ae34f590b5dc20
Rafael Guzman <rega1237@gmail.com>
2024-09-11 07:38:47 -0400

    Refactor Dialogue component UI

--- src/renderer/src/components/UI/Dialogue/Dialogue.jsx
+++ src/renderer/src/components/UI/Dialogue/Dialogue.jsx
@@ -1,31 +1,27 @@
-import useHistoriaCorporal from '../../../store/bodyHistoryStore'
-import useHistoriaFacial from '../../../store/facialHistoryStore'
-
 const Dialogue = ({ handleToggleModal, type, deleteRecord }) => {
-  const historiaFacial = useHistoriaFacial((state) => state.historiaFacial)
-  const historiaCorporal = useHistoriaCorporal((state) => state.historiaCorporal)
-  const deleteHistoriaFacial = useHistoriaFacial((state) => state.deleteHistoriaFacial)
-
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-background bg-opacity-50">
-      <div className="rounded-lg bg-primary-foreground p-5">
-        <h1 className="text-2xl font-bold">¿Estás seguro que deseas eliminar?</h1>
-        <div className="mt-5 flex justify-end gap-5">
-          <button
-            className="bg-secondary px-5 py-1 text-primary-foreground hover:bg-secondary-foreground hover:text-primary"
-            onClick={handleToggleModal}
-          >
-            Cancelar
-          </button>
-          <button
-            className="bg-secondary px-5 py-1 text-primary-foreground hover:bg-secondary-foreground hover:text-primary"
-            onClick={() => deleteRecord(type)}
-          >
-            Eliminar
-          </button>
+    <>
+      <div className="absolute top-0 z-10 min-h-full w-full bg-primary-foreground opacity-15"></div>
+      <div className="fixed inset-0 z-20 flex items-center justify-center">
+        <div className="rounded-lg bg-primary-foreground p-5">
+          <h1 className="text-2xl font-bold text-primary">¿Estás seguro que deseas eliminar?</h1>
+          <div className="mt-5 flex justify-end gap-5">
+            <button
+              className="bg-secondary px-5 py-1 text-primary-foreground hover:bg-secondary-foreground hover:text-primary"
+              onClick={handleToggleModal}
+            >
+              Cancelar
+            </button>
+            <button
+              className="bg-secondary px-5 py-1 text-primary-foreground hover:bg-secondary-foreground hover:text-primary"
+              onClick={() => deleteRecord(type)}
+            >
+              Eliminar
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
