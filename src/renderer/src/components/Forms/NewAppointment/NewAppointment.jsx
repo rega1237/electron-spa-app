16d75fd48719302da7a003424c09b81cbef8fd00
Rafael Guzman <rega1237@gmail.com>
2024-09-13 07:13:58 -0400

    Refactor NewAppointment component styling

--- src/renderer/src/components/Forms/NewAppointment/NewAppointment.jsx
+++ src/renderer/src/components/Forms/NewAppointment/NewAppointment.jsx
@@ -44,7 +44,7 @@ const Newappointment = ({ handleToggleModal }) => {
       <div className="absolute top-0 z-10 min-h-full w-full bg-primary-foreground opacity-15"></div>
       <div className="absolute top-0 z-20 w-full">
         <div className="flex h-[100vh] flex-col items-center justify-center">
-          <div className="flex w-[30%] items-center justify-between rounded-t-[10px] border-b bg-primary p-5 text-white">
+          <div className="flex items-center justify-between rounded-t-[10px] border-b bg-primary p-5 text-white">
             <h2 className="text-lg font-semibold">Nuevo Cita para {paciente.nombre_completo}</h2>
             <button className="text-muted-foreground" onClick={handleToggleModal}>
               Cerrar
