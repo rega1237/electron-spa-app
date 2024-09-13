d36ffa85a03f1210811711aeb3e2b152a3272393
Rafael Guzman <rega1237@gmail.com>
2024-09-13 07:07:38 -0400

    Add body session constants

---
+++ src/renderer/src/Constants/bodySessionConstants.js
@@ -0,0 +1,12 @@
+export const medidas = {
+  Peso: '',
+  Cintura: '',
+  Cadera: '',
+  Abdomen: '',
+  'Brazo derecho': '',
+  'Brazo izquierdo': '',
+  'Muslo derecho': '',
+  'Muslo izquierdo': '',
+  'Pantorrilla derecha': '',
+  'Pantorrilla izquierda': ''
+}
