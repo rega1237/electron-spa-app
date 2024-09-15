bac8acd68eb7facacea5749bf3e05a4fe1138487
Rafael Guzman <rega1237@gmail.com>
2024-09-15 17:37:46 -0400

    Add BirthdayCard and Birthdays components

---
+++ src/renderer/src/components/Birthdays/Birthdays.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import usePaciente from '../../store/pacienteStore'
+import BirthdayCard from './BirthdayCard/BirthdayCard'
+
+const Birthdays = () => {
+  const pacientes = usePaciente((state) => state.pacientes)
+  const [birthdays, setBirthdays] = useState([])
+
+  useEffect(() => {
+    const today = new Date()
+    const currentMonth = today.getMonth()
+
+    const birthdays = pacientes.filter((paciente) => {
+      const birthday = new Date(paciente.fecha_de_nacimiento)
+      console.log(birthday.getMonth() === currentMonth)
+      return birthday.getMonth() === currentMonth
+    })
+
+    console.log(birthdays)
+
+    setBirthdays(birthdays)
+  }, [])
+
+  return (
+    <>
+      {birthdays.map((birthday) => (
+        <div key={birthday.id} >
+          <BirthdayCard birthday={birthday} />
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default Birthdays
