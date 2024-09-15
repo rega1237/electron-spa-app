4c42e90808c5f0296e8aaba8969bd17991dd37cf
Rafael Guzman <rega1237@gmail.com>
2024-09-15 17:38:05 -0400

    Update header logo image

--- src/renderer/src/components/Header/Header.jsx
+++ src/renderer/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom'
-import solIcon from '../../assets/images/sol-pro-salud.webp'
+import logo from '../../assets/images/logo.webp'
 import Input from '../UI/Inputs/Input'
 
 const Header = ({ handleToggleModal, setSearch }) => {
@@ -8,7 +8,7 @@ const Header = ({ handleToggleModal, setSearch }) => {
   return (
     <div className="mb-6 flex items-center justify-between">
       <Link to="/" className="flex items-center gap-5">
-        <img src={solIcon} alt="Pro Salud" className="w-[100px]" />
+        <img src={logo} alt="Pro Salud" className="w-[100px]" />
         <h1 className="font-heading text-2xl font-bold">Pro Salud Spa</h1>
       </Link>
       {location.pathname === '/' && (
